2da5d618ccf085b533cf389619dcef35de7a6227
Michael Holloway <mholloway@wikimedia.org>
2018-04-16 14:44:19 -0400

    Get canonical page title from page link[rel=edit]
    Rather than attempting to parse the canonical page title from the page
    URL, get it from the page itself via the edit link element.
    
    This patch accomplishes this by adding a content script to handle messages
    from the extension to retrieve this info.

--- popup.js
+++ popup.js
@@ -14,6 +14,10 @@ function csrfFetchUrlForOrigin(origin) {
     return `${origin}/w/api.php?action=query&format=json&formatversion=2&meta=tokens&type=csrf`;
 }
 
+function getCurrentTab() {
+    return browser.tabs.query({currentWindow: true, active: true}).then(tabs => tabs[0]);
+}
+
 function getCsrfToken(origin) {
     return fetch(csrfFetchUrlForOrigin(origin), { credentials: 'same-origin' })
     .then(res => res.json())
@@ -37,27 +41,25 @@ function setUpReadingListsForUser(url) {
     .then(token => fetch(readingListSetupUrlForOrigin(url.origin, token), { method: 'POST', credentials: 'same-origin' }));
 }
 
-function parseTitleFromUrl(url) {
-    return url.searchParams.has('title') ? url.searchParams.get('title') : url.pathname.replace('/wiki/', '');
-}
-
 function show(id) {
     // Use setTimeout to work around an extension popup resizing bug on Chrome
     // see https://bugs.chromium.org/p/chromium/issues/detail?id=428044
     setTimeout(() => { document.getElementById(id).style.display = 'block' }, 50);
 }
 
-function showLoginPage(url) {
-    let loginUrl = `${url.origin}/wiki/Special:UserLogin?returnto=${encodeURIComponent(parseTitleFromUrl(url))}`;
+function showLoginPage(url, title) {
+    let loginUrl = `${url.origin}/wiki/Special:UserLogin?returnto=${encodeURIComponent(title)}`;
     if (url.search) {
         loginUrl = loginUrl.concat(`&returntoquery=${encodeURIComponent(url.search.slice(1))}`);
     }
     browser.tabs.update({ url: loginUrl });
 }
 
-function showLoginPrompt(url) {
-    document.getElementById('loginButton').onclick = (element) => showLoginPage(url);
-    show('loginPromptContainer');
+function showLoginPrompt(tab, url) {
+    return getCanonicalPageTitle(tab).then(title => {
+        document.getElementById('loginButton').onclick = (element) => showLoginPage(url, title);
+        show('loginPromptContainer');
+    });
 }
 
 function showAddToListSuccessMessage() {
@@ -74,16 +76,16 @@ function mobileToCanonicalHost(url) {
     return url;
 }
 
-function getAddToListPostBody(url) {
-    return `project=${mobileToCanonicalHost(url).origin}&title=${encodeURIComponent(parseTitleFromUrl(url))}`;
+function getAddToListPostBody(url, title) {
+    return `project=${mobileToCanonicalHost(url).origin}&title=${encodeURIComponent(title)}`;
 }
 
-function getAddToListPostOptions(url) {
+function getAddToListPostOptions(url, title) {
     return {
         method: 'POST',
         headers: { 'content-type': 'application/x-www-form-urlencoded' },
         credentials: 'same-origin',
-        body: getAddToListPostBody(url)
+        body: getAddToListPostBody(url, title)
     }
 }
 
@@ -91,21 +93,26 @@ function handleAddPageToListResult(res) {
     if (res.id) showAddToListSuccessMessage(); else showAddToListFailureMessage(res);
 }
 
-function addPageToDefaultList(url, listId, token) {
-    return fetch(readingListPostEntryUrlForOrigin(url.origin, listId, token), getAddToListPostOptions(url))
+function getCanonicalPageTitle(tab) {
+    return browser.tabs.sendMessage(tab.id, { type: 'wikiExtensionGetPageTitle' }).then(res => res.title);
+}
+
+function addPageToDefaultList(tab, url, listId, token) {
+    return getCanonicalPageTitle(tab)
+    .then(title => fetch(readingListPostEntryUrlForOrigin(url.origin, listId, token), getAddToListPostOptions(url, title)))
     .then(res => res.json())
     .then(res => handleAddPageToListResult(res));
 }
 
-function handleTokenResult(url, token) {
-    return token === '+\\' ? showLoginPrompt(url) : getDefaultListId(url).then(listId => addPageToDefaultList(url, listId, token));
+function handleTokenResult(tab, url, token) {
+    return token === '+\\' ? showLoginPrompt(tab, url) : getDefaultListId(url).then(listId => addPageToDefaultList(tab, url, listId, token));
 }
 
-function handleClick(url) {
-    return getCsrfToken(url.origin).then(token => handleTokenResult(url, token));
+function handleClick(tab, url) {
+    return getCsrfToken(url.origin).then(token => handleTokenResult(tab, url, token));
 }
 
-browser.tabs.query({currentWindow: true, active: true}).then(tabs => {
-    return handleClick(new URL(tabs[0].url))
+getCurrentTab().then(tab => {
+    return handleClick(tab, new URL(tab.url))
     .catch(err => showAddToListFailureMessage(err));
 });
