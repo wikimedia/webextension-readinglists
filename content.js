a098850432e0d962bdc61ddf3097eace0753e300
BearND <bsitzmann@wikimedia.org>
2018-04-26 22:25:17 -0600

    Hygiene: remove unused parameter, part 2

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
+browser.runtime.onMessage.addListener((message) => {
     if (message.type === 'wikiExtensionGetPageTitle') {
         return Promise.resolve({ href: document.querySelector('link[rel=canonical]').href });
     }
