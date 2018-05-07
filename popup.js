f5f1fcb9f95ccd429da2e12c04c58585dbf8543e
Michael Holloway <mholloway@wikimedia.org>
2018-05-07 12:36:33 -0400

    Fetch live messages from the API
    This lets the API select the appropriate message language for the logged-
    in user (based on the user's preference), including any applicable
    fallback logic.

--- popup.js
+++ popup.js
@@ -1,3 +1,25 @@
+const MESSAGE_KEYS = {
+    enableSync: 'readinglists-browser-enable-sync-prompt',
+    entryLimitExceeded: 'readinglists-browser-list-entry-limit-exceeded',
+    errorIntro: 'readinglists-browser-error-intro',
+    infoLinkText: 'readinglists-browser-extension-info-link-text',
+    loginButtonText: 'login',
+    loginPrompt: 'readinglists-browser-login-prompt',
+    success: 'readinglists-browser-add-entry-success'
+}
+
+const ALLMESSAGES_QUERY = {
+    action: 'query',
+    format: 'json',
+    formatversion: '2',
+    meta: 'allmessages',
+    amenableparser: ''
+};
+
+function objToQueryString(obj) {
+    return Object.keys(obj).map(key => `${key}=${obj[key]}`).join('&');
+}
+
 function getReadingListsUrlForOrigin(origin, next) {
     let result = `${origin}/api/rest_v1/data/lists/`;
     if (next) {
@@ -14,8 +36,50 @@ function csrfFetchUrlForOrigin(origin) {
     return `${origin}/w/api.php?action=query&format=json&formatversion=2&meta=tokens&type=csrf`;
 }
 
-function siteInfoUrlForOrigin(origin) {
-    return `${origin}/w/api.php?action=query&format=json&formatversion=2&meta=siteinfo`;
+function geti18nMessageUrl(origin, keys) {
+    return `${origin}/w/api.php?${objToQueryString(Object.assign(ALLMESSAGES_QUERY, { ammessages: keys.join('|') }))}`;
+}
+
+function fetchBundledMessagesForLang(lang) {
+    return fetch(browser.extension.getURL(`i18n/${lang}.json`));
+}
+
+function getBundledMessage(lang, keys) {
+    return fetchBundledMessagesForLang(lang).then(res => res.json()).then(res => {
+        const result = {};
+        keys.forEach(key => {
+            result[key] = res[key];
+        });
+        return result;
+    });
+}
+
+/**
+ * Get UI messages from the MediaWiki API (in the user's preferred UI lang), falling back to bundled
+ * English strings if this fails.
+ * @param {string} origin the origin of the site URL
+ * @param {Array[string]} keys message keys to request
+ */
+function geti18nMessages(origin, keys) {
+    return fetch(geti18nMessageUrl(origin, keys), { credentials: 'same-origin' })
+    .then(res => {
+        if (!res.ok) {
+            throw res;
+        } else {
+            return res.json();
+        }
+    })
+    .then(res => {
+        if (res.query && res.query.allmessages && res.query.allmessages.length) {
+            const result = {};
+            res.query.allmessages.forEach(messageObj => {
+                result[messageObj.name] = messageObj.content;
+            });
+            return result;
+        } else {
+            return getBundledMessage('en', keys);
+        }
+    });
 }
 
 function getCurrentTab() {
@@ -26,10 +90,6 @@ function getCsrfToken(origin) {
     return fetch(csrfFetchUrlForOrigin(origin), { credentials: 'same-origin' })
     .then(res => res.json())
     .then(res => res.query.tokens.csrftoken);
-}
-
-function getSiteInfo(origin) {
-    return fetch(siteInfoUrlForOrigin(origin)).then(res => res.json());
 }
 
 function getDefaultListId(url, next) {
@@ -81,49 +141,51 @@ function showLoginPage(url, title) {
 }
 
 function showLoginPrompt(tab, url) {
-    return getSiteInfo(url.origin)
-    .then(si => geti18nMessages(si.query.general.lang)
+    return geti18nMessages(url.origin, [ MESSAGE_KEYS.loginPrompt, MESSAGE_KEYS.loginButtonText ])
     .then(messages => getCanonicalPageTitle(tab)
     .then(title => {
-        document.getElementById('loginPromptText').textContent = messages['readinglists-browser-login-prompt'];
-        document.getElementById('loginButton').textContent = messages['login'];
+        document.getElementById('loginPromptText').textContent = messages[MESSAGE_KEYS.loginPrompt];
+        document.getElementById('loginButton').textContent = messages[MESSAGE_KEYS.loginButtonText];
         document.getElementById('loginButton').onclick = () => showLoginPage(url, title);
         show('loginPromptContainer');
-    })));
+    }));
 }
 
 function showAddToListSuccessMessage(tab, url) {
-    return getSiteInfo(url.origin)
-    .then(si => geti18nMessages(si.query.general.lang)
+    return geti18nMessages(url.origin, [ MESSAGE_KEYS.success ])
     .then(messages => getCanonicalPageTitle(tab)
     .then(title => {
-        const message = messages['readinglists-browser-add-entry-success'].replace('$1', title.replace(/_/g, ' '));
+        const message = messages[MESSAGE_KEYS.success].replace('$1', title.replace(/_/g, ' '));
         document.getElementById('successText').textContent = message;
         show('addToListSuccessContainer');
-    })));
+    }));
 }
 
 function showAddToListFailureMessage(url, res) {
-    return getSiteInfo(url.origin)
-    .then(si => geti18nMessages(si.query.general.lang)
+    return geti18nMessages(url.origin, [
+        MESSAGE_KEYS.enableSync,
+        MESSAGE_KEYS.infoLinkText,
+        MESSAGE_KEYS.entryLimitExceeded,
+        MESSAGE_KEYS.errorIntro
+    ])
     .then(messages => {
         let message;
         if (res.title === 'readinglists-db-error-not-set-up') {
-            message = messages['readinglists-browser-enable-sync-prompt'];
+            message = messages[MESSAGE_KEYS.enableSync];
             const learnMoreLink = document.getElementById('learnMoreLink');
-            learnMoreLink.textContent = messages['readinglists-browser-extension-info-link-text'];
+            learnMoreLink.textContent = messages[MESSAGE_KEYS.infoLinkText];
             learnMoreLink.onclick = () => browser.tabs.create({ url: learnMoreLink.href });
             document.getElementById('learnMoreLinkContainer').style.display = 'block';
         } else if (res.title === 'readinglists-db-error-entry-limit') {
             const maxEntries = si.query.general['readinglists-config'].maxEntriesPerList;
-            message = messages['readinglists-browser-list-entry-limit-exceeded'].replace('$1', maxEntries.toString());
+            message = messages[MESSAGE_KEYS.entryLimitExceeded].replace('$1', maxEntries.toString());
         } else {
             const detail = res.detail ? res.detail : res.title ? res.title : res.type ? res.type : typeof res === 'object' ? JSON.stringify(res) : res;
-            message = messages['readinglists-browser-error-intro'].replace('$1', detail);
+            message = messages[MESSAGE_KEYS.errorIntro].replace('$1', detail);
         }
         document.getElementById('failureReason').textContent = message;
         show('addToListFailedContainer');
-    }));
+    });
 }
 
 function mobileToCanonicalHost(url) {
@@ -165,14 +227,6 @@ function handleTokenResult(tab, url, token) {
 
 function handleClick(tab, url) {
     return getCsrfToken(url.origin).then(token => handleTokenResult(tab, url, token));
-}
-
-function fetchMessagesForLang(lang) {
-    return fetch(browser.extension.getURL(`i18n/${lang}.json`));
-}
-
-function geti18nMessages(lang) {
-    return fetchMessagesForLang(lang).then(res => res.json());
 }
 
 getCurrentTab().then(tab => {
